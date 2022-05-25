701c139c3ffe71436634b4bcffaabb2b21d08d7b
Pavel Plekhanov <75085214+Pauolosavery@users.noreply.github.com>
2022-05-25 15:37:51 +0300

    Add files via upload

---
+++ progressbar.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+// Раскомментируй только для релиза 4.
+// const runInteractiveConsole = require('./interactive');
+// runInteractiveConsole();
+
+// Асинхронная загрузка.
+function loadFiles() {
+
+}
+
+loadFiles();
